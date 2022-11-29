b8b68894f2f7f82b04e37ecdef264db794106dc5
Abinash Pun <abinashpun@Abinashs-MacBook-Pro.local>
2022-11-28 19:39:27 -0600

    scripts and test db add

--- notebooks/real_time.py
+++
@@ -1,8 +0,0 @@
-from deepface import DeepFace
-
-#DeepFace.stream(db_path='/Users/abinashpun/data_science/AI_Squad/test_ground/test_photos') #opencv
-#DeepFace.stream("dataset", detector_backend = 'opencv')
-#DeepFace.stream(db_path='/Users/abinashpun/data_science/AI_Squad/test_ground/test_photos', detector_backend = 'ssd')
-#DeepFace.stream(db_path='/Users/abinashpun/data_science/AI_Squad/test_ground/test_photos', detector_backend = 'mtcnn')
-#DeepFace.stream(db_path='/Users/abinashpun/data_science/AI_Squad/test_ground/test_photos', detector_backend = 'dlib')
-DeepFace.stream(db_path='/Users/abinashpun/data_science/AI_Squad/test_ground/test_photos', detector_backend = 'retinaface')
