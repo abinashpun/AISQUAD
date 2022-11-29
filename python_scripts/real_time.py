548cc4044b90dcdab1e9684917333983e143bc5e
Abinash Pun <abinashpun@Abinashs-MacBook-Pro.local>
2022-11-28 19:42:23 -0600

    scripts and test db add

--- python_scripts/real_time.py
+++ python_scripts/real_time.py
@@ -1,7 +1,7 @@
 from deepface import DeepFace
 
 #DeepFace.stream("dataset", detector_backend = 'opencv')
-DeepFace.stream(db_path='test_ground/test_photos',model_name ='VGG-Face', detector_backend = 'ssd', distance_metric = 'cosine', enable_face_analysis = False, source = 0, time_threshold = 5, frame_threshold = 5 )
+DeepFace.stream(db_path='test_photos',model_name ='VGG-Face', detector_backend = 'ssd', distance_metric = 'cosine', enable_face_analysis = False, source = 0, time_threshold = 5, frame_threshold = 5 )
 #DeepFace.stream(db_path='test_photos', detector_backend = 'mtcnn')
 #DeepFace.stream(db_path='test_photos', detector_backend = 'dlib')
 #DeepFace.stream(db_path='test_photos', detector_backend = 'retinaface')
